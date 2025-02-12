33e57b38864a3ec975097aba0078d5f965e2b532
saqib1790 <saqib1430@gmail.com>
2025-02-12 12:38:42 +0500

    Socket Programming LAB 3 for 1 client

---
+++ server2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+#include<sys/socket.h>
+#include<net/inet.h>
+#include<unistd.h>
+using namespace std;
+#define PORT 8080;
+int main(){
+    
+    return 0;
+}
+
+
+
+
